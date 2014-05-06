158cd72435c1a36624942a0835733772789a238e
singha5 <arunjitsingh6@gmail.com>
2014-05-06 10:06:38 -0400

    1ab

---
+++ 1 a-b/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        public static void Sum(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+
+        public static void Difference(int a, int b)
+        {
+            Console.WriteLine(a - b);
+        }
+        static void Main(string[] args)
+        {
+            int q = 30, t = 39;
+            Sum(q, t);
+            Difference(q, t);
+            product(q, t);
+            int result = product(q, t);
+            Console.WriteLine(result);
+        }
+        
+        public static int product(int a,int b)
+        {
+            return a*b;
+        }
+    }
+}
